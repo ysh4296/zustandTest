76807fddd89e7b0ba956ed18cfd42914d255b85c
cadi <dbtmdgns4296@naver.com>
2025-08-10 13:24:29 +0900

    edit nodemodules

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import useCounterStore from "./store.js";
 
 const App: React.FC = () => {
-  const store = useCounterStore(state=>{
-    return {count:state.count};
-  });
-  // useEffect(()=>{console.log(state.count);},[]);
+  // const { setValue } = useTick();
+  console.log("App component rendered");
+
+  const store = useCounterStore();
+
+  // useEffect(() => {
+  //   setValue(0);
+  //   console.log("useEffect called");
+  // }, [setValue]);
+  useEffect(() => {
+    console.log("App mounted");
+    return () => {
+      console.log("App unmounted");
+    };
+  }, []);
+
   return <h1>무한 렌더링 테스트용 프로젝트</h1>;
 };
 
-export default App; 
+const useTick = () => {
+  const [num, setNum] = useState(0);
+  const setValue = (value: number) => {
+    setNum(value);
+  };
+  useEffect(() => {
+    console.log("useTick mounted");
+    return () => {
+      console.log("useTick unmounted");
+    };
+  }, []);
+  return { num, setNum, setValue };
+};
+
+export default App;
